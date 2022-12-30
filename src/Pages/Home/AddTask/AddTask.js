9cecf38d15d458281db66283eafe4dc2036824e2
sl243 <slmiucse@gmail.com>
2022-12-30 16:25:07 +0600

    added daily task and add task

--- src/Pages/Home/AddTask/AddTask.js
+++ src/Pages/Home/AddTask/AddTask.js
@@ -4,20 +4,55 @@ import { useForm } from 'react-hook-form';
 const AddTask = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
 
-    const hanldeAddTask = data => {
 
+    const hanldeAddTask = data => {
+        const image = (data.image[0]);
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = 'https://api.imgbb.com/1/upload?expiration=600&key=c8b8f4751965741d3eaf91363d5ce7de'
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(imgData => {
+                if(imgData.success){
+                    console.log(imgData.data.url)
+
+                    const addtask = {
+                        name: data.name,
+                        image: imgData.data.url
+                    }
+
+                    // save infomation in the database
+                    fetch('http://localhost:5000/addtasks', {
+                        method: 'POST',
+                        headers: {
+                            'content-type' : 'application/json',
+                        },
+                        body: JSON.stringify(addtask)
+                    })
+                    .then( res => res.json())
+                    .then( result => {
+                        console.log(result)
+                        // toast.success(`${data.name} is added successfully`)
+                        // navigate('/dashboard/managedoctors')
+                    })
+                }
+            })
     }
+
     return (
         <div className='w-1/2 mx-auto rounded-md shadow-md mt-10'>
             <h1 className='text-2xl text-center font-bold mb-5'>Add Task</h1>
             <form onSubmit={handleSubmit(hanldeAddTask)}>
                 <div className="form-control w-3/4 mx-auto mt-3">
                     <input
-                        {...register("text", {
+                        {...register("name", {
                             required: 'Task is required'
 
                         })}
-                        type="text"
+                        type="name"
                         placeholder="Add Task"
                         className="input input-bordered w-full"
                     />
@@ -25,7 +60,7 @@ const AddTask = () => {
                 {errors.text && <p className='text-red-500 w-3/4 mx-auto'>{errors.text?.message}</p>}
                 <div className="form-control w-3/4 mx-auto mt-3">
                     <input
-                        {...register("file", {
+                        {...register("image", {
                             required: 'image is required',
 
                         })}
--- src/Pages/Home/DailyTask/DailyTask.js
+++ src/Pages/Home/DailyTask/DailyTask.js
@@ -6,18 +6,40 @@ const DailyTask = () => {
 
     const hanldeDailyTask = data => {
 
+        const task = {
+            DailyTask: data.name
+        };
+        console.log(task)
+
+        // const task = data.name
+
+        // daily task store in database
+        fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'content-type' : 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => console.error(err))
+
     }
+
     return (
         <div className='w-1/2 mx-auto rounded-md shadow-md mt-10'>
             <h1 className='text-2xl text-center font-bold mb-5'>Add Daily Task</h1>
             <form onSubmit={handleSubmit(hanldeDailyTask)}>
                 <div className="form-control w-3/4 mx-auto mt-3">
                     <input
-                        {...register("text", {
+                        {...register("name", {
                             required: 'Task is required',
 
                         })}
-                        type="text"
+                        type="name"
                         placeholder="Enter Your Daily Task"
                         className="input input-bordered w-full"
                     />
